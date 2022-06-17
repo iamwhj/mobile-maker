e42a97373483ffcea01cdb0a2991ec9ab8d98c2e
daw <962983053@qq.com>
2022-06-17 11:42:55 +0800

    feat 添加svg组件

--- front/src/commom/registerGlobalComponent.js
+++ front/src/commom/registerGlobalComponent.js
@@ -1,8 +1,10 @@
 import ActivityTopBarConfig from '@/views/activity/activityEdit/activityConfig/activityConfig';
+import SvgIcon from '@/icon/SvgIcon';
 
 // 注册全局组件
 const registerGlobalComponent = (Vue) => {
   Vue.component('ActivityConfig', ActivityTopBarConfig);
+  Vue.component('SvgIcon', SvgIcon);
 };
 
 export default registerGlobalComponent;
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service');
+const { resolve } = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 8080,
+  },
+  chainWebpack(config) {
+    // 默认加载svg规则，排除掉我们新建icon文件夹
+    config.module.rule('svg').exclude.add(resolve('src/icon'));
+
+    // 加入icon文件夹配置规则
+    config.module
+      .rule('icon')
+      .include.add(resolve('src/icon'))
+      .end()
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({ symbolId: 'icon-[name]' });
+  },
 });
