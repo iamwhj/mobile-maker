5986b7c6d07558ec648c09df742c52377278393f
daw <962983053@qq.com>
2022-06-27 17:01:26 +0800

    feat axios消息提示

--- front/src/utils/axios.js
+++ front/src/utils/axios.js
@@ -1,4 +1,5 @@
 import { create } from 'axios';
+import { ElMessage } from 'element-plus';
 
 const service = create({
   baseURL: '/api',
@@ -11,10 +12,23 @@ service.interceptors.request.use((config) => {
 
 service.interceptors.response.use(
   (response) => {
+    const data = response.data;
+    if (data.code !== 0) {
+      // 规定状态码为0则是正常请求
+      ElMessage({
+        type: 'error',
+        message: 'Response Status Code Error!',
+      });
+      return Promise.reject(data.message || 'Error');
+    }
     return response;
   },
   (err) => {
     console.log(err);
+    ElMessage({
+      type: 'error',
+      message: err,
+    });
   }
 );
 
