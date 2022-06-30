46ed779630081fac4fc4c872d30e1e2c2f91847d
daw <962983053@qq.com>
2022-06-30 23:43:18 +0800

    feat 预览服务架子

---
+++ generate/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+      },
+    ],
+  },
+  globals: {
+    defineProps: true,
+    defineEmits: true,
+    $message: true,
+  },
+};
---
+++ generate/vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './',
+  productionSourceMap: false,
+  css: {
+    extract: false,
+  },
+  configureWebpack: {
+    output: {
+      crossOriginLoading: 'anonymous',
+      filename: 'scripts/[name].[hash:8].min.js',
+      chunkFilename: 'scripts/[name].[hash:8].min.js',
+    },
+  },
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('html').tap((args) => {
+        args[0].title = '默认活动标题';
+        args[0].minify = { removeComments: false };
+        return args;
+      });
+    }
+  },
+});
