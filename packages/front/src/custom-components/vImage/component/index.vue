fc937554b95d87501d7ad8268552036800e2e2aa
daw <962983053@qq.com>
2022-07-21 18:30:45 +0800

    fix 修复图片组件点击事件

--- packages/front/src/custom-components/vImage/component/index.vue
+++ packages/front/src/custom-components/vImage/component/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="img-box" :style="{ width: width, height: height }">
+  <div
+    class="img-box"
+    :style="{ width: width, height: height }"
+    @click="clickChock"
+  >
     <img :src="src" :style="{ width: width, height: height }" />
   </div>
 </template>
 
 <script setup>
 const props = defineProps({
+  clickChock: { type: Function },
   width: { type: String, default: '240px' },
   height: { type: String, default: 'auto' },
-  src: { type: String, default: '//s1.ax1x.com/2022/07/19/j7EdyR.jpg' },
+  src: {
+    type: String,
+    default:
+      'http://121.37.148.236:3301/image/upload_2975dc2b0e680c1e425fc9f461186eb7.jpg',
+  },
 });
 </script>
 
