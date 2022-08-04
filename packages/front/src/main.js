7b1bbf2db3dc9f4e20fbef0dcab5e87c2d903d67
daw <962983053@qq.com>
2022-08-04 14:21:40 +0800

    feat front项目打包优化

--- packages/front/src/main.js
+++ packages/front/src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 import router from './router';
 import store from './store';
 import '@/style/index.scss';
@@ -9,7 +7,7 @@ import '@/plugins/echarts';
 import registerGlobalComponent from '@/commom/registerGlobalComponent';
 import registerCustomComponents from './custom-components';
 
-const app = createApp(App).use(store).use(router).use(ElementPlus);
+const app = createApp(App).use(store).use(router);
 
 // 自定义组件全局注册
 registerGlobalComponent(app);
