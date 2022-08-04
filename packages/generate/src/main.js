e9b377ce478fc242f01e795a4d8d6a840cfc64a0
daw <962983053@qq.com>
2022-08-04 14:21:56 +0800

    feat generate项目打包优化

--- packages/generate/src/main.js
+++ packages/generate/src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import '@/style/index.scss';
 import '@/common/rem';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 import '@/plugins/echarts';
 import registerCustomComponents from './custom-components';
 
-const app = createApp(App).use(ElementPlus);
+const app = createApp(App);
 
 // 注册组件
 registerCustomComponents(app);
--- packages/generate/src/plugins/echarts.js
+++ packages/generate/src/plugins/echarts.js
@@ -8,11 +8,8 @@ import {
   TooltipComponent,
   GridComponent,
   DatasetComponent,
-  TransformComponent,
   LegendComponent,
 } from 'echarts/components';
-// 标签自动布局，全局过渡动画等特性
-import { LabelLayout, UniversalTransition } from 'echarts/features';
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
 import { CanvasRenderer } from 'echarts/renderers';
 
@@ -22,11 +19,8 @@ echarts.use([
   TooltipComponent,
   GridComponent,
   DatasetComponent,
-  TransformComponent,
   LegendComponent,
   PieChart,
-  LabelLayout,
-  UniversalTransition,
   CanvasRenderer,
 ]);
 
