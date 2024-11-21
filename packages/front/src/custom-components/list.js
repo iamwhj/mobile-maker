a1653d575c82a0393bc5664deacb030a0b56a8e3
Daw <962983053@qq.com>
2024-11-21 12:55:08 +0800

    feat 图标更新

--- packages/front/src/custom-components/list.js
+++ packages/front/src/custom-components/list.js
@@ -20,31 +20,31 @@ export const localComponentList = [
         "id": 1,    // id，唯一
         "name": "vText",    // 组件命名key，唯一，需要和组件注册名一致
         "full_name": "文字",    // 组件名称
-        "path": require('../assets/文字.png'),  // 组件图标路径
+        "path": "https://github.com/iamwhj/picx-images-hosting/raw/master/mobile-maker/文字.60u9vk9ca0.webp",  // 组件图标路径
         "category_id": 1,   // 组件分类 1基础组件
     }, {
         "id": 2,    // id，唯一
         "name": "vButton",    // 组件命名key，唯一，需要和组件注册名一致
         "full_name": "按钮",    // 组件名称
-        "path": require('../assets/按钮.png'),  // 组件图标路径
+        "path": "https://github.com/iamwhj/picx-images-hosting/raw/master/mobile-maker/按钮.2yyduc802v.webp",  // 组件图标路径
         "category_id": 1,   // 组件分类 1基础组件
     }, {
         "id": 3,    // id，唯一
         "name": "vImage",    // 组件命名key，唯一，需要和组件注册名一致
         "full_name": "图片",    // 组件名称
-        "path": require('../assets/图片.png'),  // 组件图标路径
+        "path": "https://github.com/iamwhj/picx-images-hosting/raw/master/mobile-maker/图片.1e8muvasmf.webp",  // 组件图标路径
         "category_id": 1,   // 组件分类 1基础组件
     }, {
         "id": 4,    // id，唯一
         "name": "pieChart",    // 组件命名key，唯一，需要和组件注册名一致
         "full_name": "饼图",    // 组件名称
-        "path": require('../assets/按钮.png'),  // 组件图标路径
+        "path": "https://github.com/iamwhj/picx-images-hosting/raw/master/mobile-maker/饼图.2verwlvtmn.webp",  // 组件图标路径
         "category_id": 2,   // 组件分类
     }, {
         "id": 5,    // id，唯一
         "name": "time",    // 组件命名key，唯一，需要和组件注册名一致
         "full_name": "倒计时",    // 组件名称
-        "path": require('../assets/按钮.png'),  // 组件图标路径
+        "path": "https://github.com/iamwhj/picx-images-hosting/raw/master/mobile-maker/时间.41y3583740.webp",  // 组件图标路径
         "category_id": 3,   // 组件分类
     }
 ]
